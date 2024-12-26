bc8ea1fcb8da94cd9ba561b1dd7c3351629313f3
vaishali <vaishalichauhan2801@gmail.com>
2024-12-25 21:18:51 -0800

    complete

---
+++ que5.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main(){
+	
+	int i,j;
+	
+	 for(i=5; i>=1; i--){
+		
+		for(j=1; j<i; j++){
+			  
+				printf("  ");
+			    }
+		for(j=i; j<=5; j++){
+				
+				printf("%d ", j);
+				
+			    }
+		for(j=5-1; j>=i; j--){
+				
+				printf("%d ", j);
+				
+			    }
+			
+		printf("\n");
+		
+	}
+	
+	return 0;
+}
+
+
+
+  
---
+++ que7.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+	
+	int i,j;
+	
+    for(i=0 ;i<=5 ;i++){
+    	
+    			if(i == 0 || i == 2 ){
+    				printf("* * * * * \n");
+				}
+				else if(i == 1){
+					printf("*       * \n");
+				}
+				else{
+					printf("* \n");
+				}	
+     
+	            }
+	return 0;
+	
+}
